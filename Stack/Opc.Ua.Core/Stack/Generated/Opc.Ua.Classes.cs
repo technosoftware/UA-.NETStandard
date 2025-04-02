d45b5aa95eeeb7b10902a90dd548f6a3dc466df3
Randy Armstrong <randy@sparhawksoftware.com>
2025-04-02 06:00:30 -0700

    Fixes ValueRank for subtypes of DataTypeType.

--- Stack/Opc.Ua.Core/Stack/Generated/Opc.Ua.Classes.cs
+++ Stack/Opc.Ua.Core/Stack/Generated/Opc.Ua.Classes.cs
@@ -40880,7 +40880,7 @@ namespace Opc.Ua
         /// <remarks />
         protected override int GetDefaultValueRank()
         {
-            return ValueRanks.Scalar;
+            return ValueRanks.Any;
         }
 
         #if (!OPCUA_EXCLUDE_InitializationStrings)
@@ -41199,7 +41199,7 @@ namespace Opc.Ua
         /// <remarks />
         protected override int GetDefaultValueRank()
         {
-            return ValueRanks.Scalar;
+            return ValueRanks.Any;
         }
 
         #if (!OPCUA_EXCLUDE_InitializationStrings)
@@ -41321,7 +41321,7 @@ namespace Opc.Ua
         /// <remarks />
         protected override int GetDefaultValueRank()
         {
-            return ValueRanks.Scalar;
+            return ValueRanks.Any;
         }
 
         #if (!OPCUA_EXCLUDE_InitializationStrings)
@@ -41444,7 +41444,7 @@ namespace Opc.Ua
         /// <remarks />
         protected override int GetDefaultValueRank()
         {
-            return ValueRanks.Scalar;
+            return ValueRanks.Any;
         }
 
         #if (!OPCUA_EXCLUDE_InitializationStrings)
@@ -41568,7 +41568,7 @@ namespace Opc.Ua
         /// <remarks />
         protected override int GetDefaultValueRank()
         {
-            return ValueRanks.Scalar;
+            return ValueRanks.Any;
         }
 
         #if (!OPCUA_EXCLUDE_InitializationStrings)
@@ -41887,7 +41887,7 @@ namespace Opc.Ua
         /// <remarks />
         protected override int GetDefaultValueRank()
         {
-            return ValueRanks.Scalar;
+            return ValueRanks.Any;
         }
 
         #if (!OPCUA_EXCLUDE_InitializationStrings)
@@ -45475,7 +45475,7 @@ namespace Opc.Ua
         /// <remarks />
         protected override int GetDefaultValueRank()
         {
-            return ValueRanks.Scalar;
+            return ValueRanks.Any;
         }
 
         #if (!OPCUA_EXCLUDE_InitializationStrings)
